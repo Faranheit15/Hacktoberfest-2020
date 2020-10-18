d2ddbd985a89acdc1d68b4ecf8ee01430e67c972
Niraj Dhalani <32194632+nirajdhalani@users.noreply.github.com>
2020-10-18 20:17:32 +0530

    Create printwithoutnewline.py
    Print without newline in Python 3.x

---
+++ printwithoutnewline.py
@@ -0,0 +1,15 @@
+# Python 3 code for printing 
+# on the same line printing 
+# geeks and geeksforgeeks 
+# in the same line 
+
+print("geeks", end =" ") 
+print("geeksforgeeks") 
+
+# array 
+a = [1, 2, 3, 4] 
+
+# printing a element in same 
+# line 
+for i in range(4): 
+	print(a[i], end =" ") 
