68cba49dbff21c7c745830aae85cfee9f6962c0f
ankita080208 <52229554+ankita080208@users.noreply.github.com>
2020-10-31 23:56:18 +0530

    Update append.py
    did minor changes.

--- Programs/append.py
+++ Programs/append.py
@@ -1,3 +1,11 @@
+#create a list.
 myList = ["Deepak", "Faran"]
+#now, if user wish to add more elements in list then uses append function.
 myList.append("Fazal")
+myList.append("abc")
+myList.append("123")
+myList.append("rosy")
+myList.append("456")
+#printing list after append.
 print(myList)
+#end of code.
